b365a00abf1dcd7a8c33147bf2faddf878d9a1b8
Helle Haug <helle.haug@visma.com>
2022-06-24 14:36:45 +0200

    :ambulance:

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,7 +1,4 @@
 package model;
-
-import java.awt.Point; // bruker bare denne strukturen før vi får orden på noe annet
-
 public class Task {
 
     // initierer med alle attributtene fra orginal ORP, så får vi heller se an hva vi fjerner underveis 
@@ -13,18 +10,37 @@ public class Task {
     private int timeDependentOffsetInterval;
     private int weight;
     private boolean requirePhysicalAppearance;
-    private Point location;
+    private Location location;
     private short id;
     private boolean prioritized;
 
-    /**
-     *  Transport type = 0 if driving was used to this task, and 1 if walking was used to this task
-     *  Transported by refers to the id of the shift that transported you here (i.e., carpooling was used)
-     */
-
-    // 0 = drive, 1 = walk
-    private int transportType;
-    private int transportedBy;
-
     public Task(){};
+    
+    // Setters 
+    
+    public void setTimeDependentOfSetInterval(int time) { this.timeDependentOffsetInterval = time;}
+    
+    // Getters
+
+    public int getId(){ return this.id; }
+
+    public int getStartTime(){ return this.startTime; }
+
+    public int getEndTime(){ return this.endTime; }
+
+    public int getTaskDuration(){ return this.duration; }
+
+    public int getIsTimeDependentOfSetInterval(){ return this.timeDependentOffsetInterval; }
+
+    public int getWeight(){ return this.weight; }
+
+    public boolean isStrict(){ return this.isStrict; }
+
+    public boolean isTimeDependent(){ return this.isTimeDependent; }
+
+    public boolean requirePhysicalAppearance(){ return this.requirePhysicalAppearance; }
+
+    public boolean isPrioritized(){ return this.prioritized; }
+
+    public Location getLocation(){ return this.location; }
 }
