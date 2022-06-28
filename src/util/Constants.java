8cde4c382d922adfb76c375cb43ee71f4faca8eb
Hans Markus <hans.markus@visma.com>
2022-06-28 14:46:26 +0200

    :sparkles: Constants for Transport mode

---
+++ src/util/Constants.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Constants {
+
+    static class TransportMode {
+        public static
+    }
+
+    public static final int TRANSPORT_MODE_WALK = 1;
+
+    public static final int TRANSPORT_MODE_DRIVE = 0;
+
+
+}
