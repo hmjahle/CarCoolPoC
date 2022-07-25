ecb5911a8a15b01139fb6e13ae0a195492cc29d7
solveigMohr <solveig.mohr@visma.com>
2022-07-25 15:24:30 +0200

    :fire: asumes all tasks are compatible

--- src/algorithm/operators/OperatorAbstract.java
+++ src/algorithm/operators/OperatorAbstract.java
@@ -21,28 +21,6 @@ public abstract class OperatorAbstract {
         this.random = random;
     }
 
-
-    public boolean canBeMerged(Model model, Shift shift, List<Task> currentTasks, List<Task> mergeTasks) {
-        // Assumes all tasks are compatible
-        return true;
-    }
-
-
-    public static boolean adheresToModelConventions(Model model, List<Task> route, Shift shift) {
-        return isCompatibleWithTask(model, route, shift) &&
-                !containsIncompatibleTasks(model, route);
-    }
-
-    private static boolean isCompatibleWithTask(Model model, List<Task> route, Shift shift) {
-        // Assume all tasks and shifts are compatible
-        return true;
-    }
-
-    private static boolean containsIncompatibleTasks(Model model, List<Task> route) {
-        // Assume no tasks are incompatible
-        return false;
-    }
-
     public static double noise(Random random) {
         return (1 - MAX_NOISE_ON_OBJECTIVE_VALUE / 2) + MAX_NOISE_ON_OBJECTIVE_VALUE * random.nextDouble();
     }
