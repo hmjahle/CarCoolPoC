be28fd9f8e5252013a811d291a2a87059969a582
solveigMohr <solveig.mohr@visma.com>
2022-08-02 15:23:33 +0200

    :sparkles: create class for results during greedy repair

---
+++ src/routeEvaluator/results/InsertVisitResult.java
@@ -0,0 +1,75 @@
+package routeEvaluator.results;
+
+import java.util.List;
+import java.util.Map;
+
+import model.Shift;
+import model.TimeDependentVisitPair;
+import model.Visit;
+import solution.Objective;
+
+public class InsertVisitResult {
+
+    private RouteEvaluatorResult routeEvaluatorOne;
+    private RouteEvaluatorResult routeEvaluatorTwo;
+    private Shift shiftIdOne;
+    private Shift shiftIdTwo;
+    private List<TimeDependentVisitPair> newTimeDependentVisitPairs;
+    private Map<Visit, Integer> carpoolSyncedVisitStartTime;
+    private boolean multipleRoutesAffected = false;
+
+    public InsertVisitResult(RouteEvaluatorResult routeEvaluatorOne, RouteEvaluatorResult routeEvaluatorTwo,
+            List<TimeDependentVisitPair> newTimeDependentVisitPairs,
+            Map<Visit, Integer> carpoolTimeDependentVisitStartTime,
+            Shift shiftIdOne, Shift shiftIdTwo) {
+        this.routeEvaluatorOne = routeEvaluatorOne;
+        this.routeEvaluatorTwo = routeEvaluatorTwo;
+        this.newTimeDependentVisitPairs = newTimeDependentVisitPairs;
+        this.carpoolSyncedVisitStartTime = carpoolTimeDependentVisitStartTime;
+        this.multipleRoutesAffected = true;
+        this.shiftIdOne = shiftIdOne;
+        this.shiftIdTwo = shiftIdTwo;
+    }
+
+    public InsertVisitResult(RouteEvaluatorResult routeEvaluatorOne, Shift shiftIdOne) {
+        this.routeEvaluatorOne = routeEvaluatorOne;
+        this.shiftIdOne = shiftIdOne;
+    }
+
+    public Map<Visit, Integer> getCarpoolSyncedVisitStartTime() {
+        return carpoolSyncedVisitStartTime;
+    }
+
+    public RouteEvaluatorResult getRouteEvaluatorOne() {
+        return routeEvaluatorOne;
+    }
+
+    public RouteEvaluatorResult getRouteEvaluatorTwo() {
+        return routeEvaluatorTwo;
+    }
+
+    public List<TimeDependentVisitPair> getNewTimeDependentVisitPairs() {
+        return newTimeDependentVisitPairs;
+    }
+
+    public boolean isMultipleRoutesAffected() {
+        return multipleRoutesAffected;
+    }
+
+    public Double getDeltaObjective(Objective previousObjective){
+        if (isMultipleRoutesAffected()){
+            return (routeEvaluatorOne.getObjectiveValue()-previousObjective.getShiftIntraRouteObjectiveValue(shiftIdOne)) + (routeEvaluatorTwo.getObjectiveValue() - previousObjective.getShiftIntraRouteObjectiveValue(shiftIdTwo));
+        }
+        return (routeEvaluatorOne.getObjectiveValue()-previousObjective.getShiftIntraRouteObjectiveValue(shiftIdOne));
+    }
+
+    public boolean isInfeasibleInsert(){
+        if (isMultipleRoutesAffected()){
+            return routeEvaluatorOne == null || routeEvaluatorTwo == null;
+        }
+        return routeEvaluatorOne == null;
+    }
+
+    
+
+}
